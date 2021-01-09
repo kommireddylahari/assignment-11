e3c968479a9eb4d982e5a125adb92cfa8bd46b94
kommireddylahari <77184373+kommireddylahari@users.noreply.github.com>
2021-01-09 10:28:51 +0530

    Add files via upload

---
+++ c code/main (1).c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    char v1;
+    char t;
+    char t1;
+    char T;
+    char probablity;
+    char v;
+    probablity=0.3;
+    v=0.33;
+    T=t1/t;
+    T=0.15;
+    v1=(1-(T)*probablity*v);
+    printf("voltage at x=%c",v1);
+    return 0;
+}
